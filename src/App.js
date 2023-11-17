3f162c43b271bbf54b2346888fd18cfb55a61f63
KirtiPatel79 <kirtipateljio@gmail.com>
2023-11-17 20:12:24 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [currentPage, setCurrentPage] = useState('urlShortener');
   return (
     <div className='h-screen justify-center items-center bg-gradient-to-r from-violet-200 to-fuchsia-200 bg-opacity-25'>
-    <div className='flex flex-row justify-center items-center '>
+    <div className='flex flex-wrap justify-center items-center '>
      
       <button className={`mt-5 border-2 border-blue-500 text-blue-500 font-medium px-5 py-2 ml-4 rounded-md hover:bg-blue-500 hover:text-white ${currentPage === 'urlShortener' ? 'bg-blue-500 text-yellow-300' : ''}`} onClick={() => setCurrentPage('urlShortener')}>Url Shortener</button>
       
